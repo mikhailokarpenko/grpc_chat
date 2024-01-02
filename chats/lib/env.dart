7ca45efb15753e72b84ba337c8e2395db7b406e6
Mike <qweqwenet@gmail.com>
2024-01-02 00:00:13 -0500

    prepared chats service for deploy

--- chats/lib/env.dart
+++ chats/lib/env.dart
@@ -1,6 +1,6 @@
 import 'dart:io' show Platform;
 
 abstract class Env {
-  static int port = int.parse(Platform.environment['PORT'] ?? '4402');
-  static String sk = Platform.environment['SK'] ?? 'secret_key';
+  static int port = int.parse(Platform.environment['PORT']!);
+  static String sk = Platform.environment['SK']!;
 }
